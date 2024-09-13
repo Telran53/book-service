1dd44fdb1170e8ac08f8a36f950f0861b23e5fb8
edward <edwardlfish@gmail.com>
2024-09-13 10:45:27 +0300

    bidirectional association

--- src/main/java/telran/java53/book/model/Publisher.java
+++ src/main/java/telran/java53/book/model/Publisher.java
@@ -1,9 +1,13 @@
 package telran.java53.book.model;
 
 import java.io.Serializable;
+import java.util.Set;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -16,12 +20,20 @@ import lombok.Setter;
 @Setter
 @EqualsAndHashCode(of = "publisherName")
 @Entity
-public class Publisher implements Serializable{
+@Table(name = "publisher")
+public class Publisher implements Serializable {
 	private static final long serialVersionUID = -4634176458163835044L;
-	
+
 	@Id
+	@Column(name = "publisher_name")
 	String publisherName;
-	
+	@OneToMany(mappedBy = "publisher")
+	Set<Book> books;
+
+	public Publisher(String publisherName) {
+		this.publisherName = publisherName;
+	}
+
 	@Override
 	public String toString() {
 		return publisherName;
